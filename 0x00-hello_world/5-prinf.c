666f4b85fd6b3f78b2e455524be7f93a79c05636
Techie-Ken <kennedyokekesuites@gmail.comgit config --global user.id Techie-Ken>
2022-07-04 14:11:44 -0700

    Task 5

---
+++ 0x00-hello_world/5-prinf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - m first C basic program
+ * Return: 0
+ */
+
+
+int main(void)
+
+
+
+{
+
+	printf("\with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+
+}
