d02e3ab7b667493e942a99591718525763f87f21
Techie-Ken <kennedyokekesuites@gmail.comgit config --global user.id Techie-Ken>
2022-07-04 12:03:44 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - my first C basic program
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+
+}
+
+
