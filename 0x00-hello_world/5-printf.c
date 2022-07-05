d47e4d8ae2dc400fae1879bc96e46540c74936c6
Techie-Ken <kennedyokekesuites@gmail.comgit config --global user.id Techie-Ken>
2022-07-04 19:30:10 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
 
-
-
 /**
- * main - m Entry point
+ * main - Entry point
  * Return: 0
  */
 
-
 int main(void)
 
-
-
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
-
 }
