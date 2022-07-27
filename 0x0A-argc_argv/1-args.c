eb2532ab635f1d3feda7ab66b3c614ae74a1ecae
Techie-Ken <kennedyokekesuites@gmail.comgit config --global user.id Techie-Ken>
2022-07-27 16:28:31 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print no argument
+ * @argc: no of arguments
+ * @argv: the array
+ * Return: 0 
+ */
+
+int main(int argc, char *argv[]_attribute_((unused)))
+{
+	print("%s\n", argc - 1)
+		return (0);
+}
