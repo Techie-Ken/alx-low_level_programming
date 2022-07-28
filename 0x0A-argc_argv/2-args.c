8386520bbdea5ae6e11d7544c4728393577f5aef
Techie-Ken <kennedyokekesuites@gmail.comgit config --global user.id Techie-Ken>
2022-07-27 17:14:15 -0700

    Task 1

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of paramethers
+ * @argv: string of parameters
+ * Return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a = 0;
+
+	while (i < argv)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
